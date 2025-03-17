ffb193c7d528cf2976a23b89fadbda2261051259
dbenavidess <dbenavidess452@gmail.com>
2025-03-17 14:26:40 -0500

    initial commit

---
+++ src/main/java/com/dbenavidess/gym_part_1/GymPart1Application.java
@@ -0,0 +1,13 @@
+package com.dbenavidess.gym_part_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymPart1Application.class, args);
+	}
+
+}
